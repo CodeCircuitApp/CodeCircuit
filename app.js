e30621431ecf7b23235d2e7dbb692d896284caa2
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-03-21 14:45:55 +0100

    make app accessible in the local network

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.get("/", (req, res) => {
 
 app.use("/events", eventsRouter);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`);
 });
